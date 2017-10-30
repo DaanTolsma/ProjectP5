4be67c8b8a7e2ab7213fc2ce6a740760c7c94d26
lems1502 <lems1502@student.nhl.nl>
2017-10-30 23:48:48 +0100

    buff images toegevoegt. moet nog verschijnen en verdwijnen

--- AI/Game2.0/js/BuffSpawnpoint.js
+++ AI/Game2.0/js/BuffSpawnpoint.js
@@ -42,16 +42,28 @@ class BuffSpawnpoint {
                 }
                 if(this.buff.getDmgBoost != 0 && this.buff != null){                //bij deze statements kan null uitkomen wanneer de
                     player.setDmgBoost(this.buff.getDmgBoost,this.buff.getDuration);//voorgaande statement is uitgevoerd, dit is geen probleem
-                    this.buff.removeBuff();                                         //want het zal nooit in de andere statemens komen.
+                    scorecounter += 50;                                             //want het zal nooit in de andere statemens komen.
+                    var elem = document.getElementById('score');
+                    elem.innerHTML = "Score: " + scorecounter;
+                    var showbuff = document.getElementById('damageboost');
+                    showbuff.style.display = 'block';                     
+                    this.buff.removeBuff();                                         
                     this.buff = null;
+                    showbuff.style.display = 'none';    
                 }
                 if(this.buff.getSpeedBoost != 0 && this.buff != null){
                     player.setSpeedBoost(this.buff.getSpeedBoost,this.buff.getDuration);
+                    scorecounter += 50;
+                    var elem = document.getElementById('score');
+                    elem.innerHTML = "Score: " + scorecounter;
                     this.buff.removeBuff();
                     this.buff = null;
                 }
                 if(this.buff.getHealth > 0 && player.getHealth < player.getMaxHealth && this.buff != null){
                     var finalhealth = player.getHealth + this.buff.getHealth;
+                    scorecounter += 50;
+                    var elem = document.getElementById('score');
+                    elem.innerHTML = "Score: " + scorecounter;
                     if(finalhealth > player.getMaxHealth){
                         finalhealth = player.getMaxHealth;
                     }
